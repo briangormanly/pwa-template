98efe6aabbe47f4f4cfd2d9b45f996e73421ac5d
Brian Gormanly <bgormanly@gmail.com>
2018-09-18 16:25:07 -0400

    working service worker demo

--- sw.js
+++ sw.js
@@ -1,5 +1,26 @@
-if(navigator.serviceWorker) {
-  navigator.serviceWorker.register('/sw.js').then(function(registration) {
-    console.log('SW Registered!');
-  }).catch(console.log);
-}
+self.addEventListener('install', event => {
+  console.log('V1 installing…');
+
+  // cache a cat SVG
+/*
+  event.waitUntil(
+    caches.open('static-v1').then(cache => cache.add('/cat.svg'))
+  );
+  */
+});
+
+self.addEventListener('activate', event => {
+  console.log('V1 now ready to handle fetches!');
+});
+
+self.addEventListener('fetch', event => {
+  const url = new URL(event.request.url);
+
+  // serve the cat SVG from the cache if the request is
+  // same-origin and the path is '/dog.svg'
+/*
+  if (url.origin == location.origin && url.pathname == '/dog.svg') {
+    event.respondWith(caches.match('/cat.svg'));
+  }
+  */
+});
